9535bf3b3cbe0407de28a80436af0a27d1557dcc
take1917 <tetsu0224.soccer@icloud.com>
2024-10-31 00:18:55 +0900

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+        <p className="text-red-500">こんにちは、これから勉強を始めます。</p>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
